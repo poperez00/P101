03ea0a5a9c2b7ff308322f6d273e9297a656febf
estudiante <estudiante@01DAW01>
2022-02-23 13:39:06 +0100

    Nueva modificación

--- src/com/edu/Principal.java
+++ src/com/edu/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola que tal");
+		System.out.println("Hola que tal, esto se ha añadido nuevo");
 
 	}
 
